593f5e4ea442c9bc229b575cb46d3cded83888ab
Valdemir Filho <filhux@gmail.com>
2021-08-20 21:26:19 -0300

    Add http response

--- index.js
+++ index.js
@@ -27,3 +27,15 @@ bot.on('message', (msg) => {
     });
   }
 });
+
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n')
+});
+
+server.listen(3000, () => {
+  console.log("Server running!");
+})
